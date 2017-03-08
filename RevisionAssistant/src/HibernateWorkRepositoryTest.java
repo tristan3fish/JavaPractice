c4057683ca1dbbcf28e8b0a5f3c0fd60d9aab4a6
tristan phillips <tristan3fish@hotmail.com>
2017-03-08 16:46:42 +0000

    attempt at a unit test

---
+++ RevisionAssistant/src/HibernateWorkRepositoryTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import junit.framework.TestCase;
+
+public class HibernateWorkRepositoryTest extends TestCase {
+	   @Test
+	   public void testPrintMessage() {
+	      assertEquals(1,1);
+	   }
+}
+
+
